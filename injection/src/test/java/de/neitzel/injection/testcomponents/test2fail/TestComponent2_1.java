8fbd37c215ae033d2baafd2f8b843708b331b9fd
Konrad Neitzel <konrad@kneitzel.de>
2025-12-14 13:51:15 +0100

    Switching to jakarta inject api elements.

--- injection/src/test/java/de/neitzel/injection/testcomponents/test2fail/TestComponent2_1.java
+++ injection/src/test/java/de/neitzel/injection/testcomponents/test2fail/TestComponent2_1.java
@@ -1,11 +1,11 @@
 package de.neitzel.injection.testcomponents.test2fail;
 
-import de.neitzel.injection.annotation.Component;
+import jakarta.inject.Singleton;
 
 /**
  * TestComponent1 that should fail.
  */
-@Component
+@Singleton
 public class TestComponent2_1 {
     public TestComponent2_1(String test) {
     }
